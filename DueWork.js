3afe2cd31ce1e7fe4011e5ae19ee6743e6a0a694
Zac McWilliam <zac@mcwilliam.net.au>
2022-02-14 15:27:23 +1100

    WriteCache is also in AllPages.js

--- DueWork.js
+++ DueWork.js
@@ -1,31 +1,10 @@
 var parser = new DOMParser();
 var regExp = /\(([^)]+)\)/;
-async function OnLoad() {
-    if (window.location.pathname.startsWith("/learning/due/")) {
-        await WriteCache()
-        setInterval(AfterLoad, 1000)
-    }
+
+if (window.location.pathname.startsWith("/learning/due/")) {
+    setInterval(AfterLoad, 1000)
 }
-OnLoad()
-async function WriteCache() {
-    const result = localStorage.getItem('cache')
-    if (!result) {
-        fetch('https://learning.stmichaels.vic.edu.au/timetable').then(r => r.text()).then(result => {
-            const timetable = parser.parseFromString(result, 'text/html')
-            let classnames = []
-            for (const classtime of timetable.getElementsByClassName("timetable-subject")) {
-                if (classtime.style.backgroundColor && classtime.childNodes[1].nodeName == "A") {
-                    const classname = classtime.childNodes[1].href.split("/")[classtime.childNodes[1].href.split("/").length-1]
-                    localStorage.setItem(classname, classtime.style.backgroundColor)
-                    classnames.push(classname)
-                }
-            }
-            localStorage.setItem("cacheClasses", [...new Set(classnames)])
-            localStorage.setItem("cache", Date.now())
-        })
-    }
-    return "done"
-}
+
 function AfterLoad() {
     for (const duework of document.getElementsByClassName("event-container")) {
         const classcodes = regExp.exec(duework.querySelector("span.fc-event-title").innerText)[1].split(",")
