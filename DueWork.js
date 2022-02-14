77bd89bdd0eb10618ec39e208eb54d7ca6b74d43
Cowbiler <sebgtaylor@gmail.com>
2022-02-14 22:27:31 +1100

    Duework comments

--- DueWork.js
+++ DueWork.js
@@ -1,5 +1,6 @@
 window.onload = function () {
     if (window.location.pathname.startsWith("/learning/due")) {
+        //The delay is due that duework has this weird thing (that no other pages do) where it gets the duework items after serving html
         setInterval(DueWork, 1000)
     }
 }
@@ -7,11 +8,13 @@ window.onload = function () {
 function DueWork() {
     var regExp = /\(([^)]+)\)/;
     for (const duework of document.getElementsByClassName("event-container")) {
+        //Same reason as #MainPage.js, support for multiple merged classes
         const classcodes = regExp.exec(duework.querySelector("span.fc-event-title").innerText)[1].split(",")
         for (const classcode of classcodes) {
             const color = localStorage.getItem(classcode)
             duework.style.backgroundColor = color
             for (const title of duework.children) {
+                //White on the light colours are really hard to read, so black text it is
                 title.style.color = "black"
             }
         }
