8f15438bb99da8967d0780701516363a37265aa6
Cowbiler <sebgtaylor@gmail.com>
2022-02-16 23:07:58 +1100

    Removed durwork comment

--- compiled.js
+++ compiled.js
@@ -120,29 +120,7 @@ async function WriteCache() {
             //Cache is in unix for recaching
             localStorage.setItem("cache", Date.now())
         })
-        //TODO: Maybe get back to this one day when you have worked out how due work started
-        // //REPLACE THIS!!! WAY TOO HACKY ACCESS USERDETAILS SOMEHOW
-        // const usersrc = document.getElementById("profile-drop").querySelector("img").src
-        // const userid = usersrc.split("?")[1].split("&")[0].split("=")[1]
-        // console.log(`/learning/due/${userid}?start=${Date.now()-twoweeks}&end=${Date.now()+twoweeks}`)
-        // fetch(`/learning/due/${userid}?start=${Date.now()-twoweeks}&end=${Date.now()+twoweeks}`).then(r => r.text()).then(result => {
-        //     console.log(result)
-        //     // const timetable = parser.parseFromString(result, 'text/html')
-        //     // for (const classtime of timetable.getElementsByClassName("timetable-subject")) {
-        //     //     //Only items with links are loaded here
-        //     //     if (classtime.style.backgroundColor && classtime.childNodes[1].nodeName == "A") {
-        //     //         const classname = classtime.childNodes[1].href.split("/")[classtime.childNodes[1].href.split("/").length-1]
-        //     //         localStorage.setItem(classname, classtime.style.backgroundColor)
-        //     //     }
-        //     //     //Timetables without links are here (EG sport, private periods)
-        //     //     else if (classtime.style.backgroundColor && classtime.childNodes[1].nodeName == "DIV") {
-        //     //         const classname = regExp.exec(classtime.childNodes[1].textContent.split("\n")[0])[1]
-        //     //         localStorage.setItem(classname, classtime.style.backgroundColor)
-        //     //     }
-        //     // }
-        //     // //Cache is in unix for recaching
-        //     // localStorage.setItem("cache", Date.now())
-        // })
+       
     }
     //Async so other things do not try to access colours before this is done
     return "done"
