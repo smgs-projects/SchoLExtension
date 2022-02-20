aace0e7deba099885db110a1b83f3a861ab7c232
Cowbiler <sebgtaylor@gmail.com>
2022-02-20 21:45:18 +1100

    Added reset colours button

--- ColourChange.js
+++ ColourChange.js
@@ -22,10 +22,20 @@ function ProfilePage() {
             parentelement.querySelector("dl").appendChild(classColourAccordian)
         }
     }
+    const classColourAccordian = document.createElement("dd")
+    classColourAccordian.innerHTML = "<a id='resetColours' style='background-color: #ff6961' aria-expanded='false'>Reset Class Colours</a>"
+    classColourAccordian.querySelector("a").addEventListener('click', ResetColours);
+    parentelement.querySelector("dl").appendChild(classColourAccordian)
     document.getElementById("content").appendChild(parentelement)
+}
+async function ResetColours() {
+    localStorage.removeItem("cache-Colour")
+    await WriteCache()
+    UpdateColours()
 }
 function UpdateColours() {
     for (const tag of document.getElementById("accordion-main-content").querySelectorAll("a")) {
+        if (tag.id === "resetColours") return;
         tag.style.backgroundColor = localStorage.getItem(tag.textContent)
     }
     AllPages()
@@ -35,4 +45,31 @@ function UpdateLocalStorage(item) {
     const classname = item.target.id.split("panelinput")[1]
     localStorage.setItem(classname, colour)
     UpdateColours()
+}
+async function WriteCache() {
+    //Needed since the fetch returns string
+    var parser = new DOMParser();
+    const result = localStorage.getItem('cache-Colour')
+    if (!result) {
+        fetch('/timetable').then(r => r.text()).then(result => {
+            const timetable = parser.parseFromString(result, 'text/html')
+            for (const classtime of timetable.getElementsByClassName("timetable-subject")) {
+                //Only items with links are loaded here
+                if (classtime.style.backgroundColor && classtime.childNodes[1].nodeName == "A") {
+                    const classname = classtime.childNodes[1].href.split("/")[classtime.childNodes[1].href.split("/").length - 1]
+                    localStorage.setItem(classname, classtime.style.backgroundColor)
+                }
+                //Timetables without links are here (EG sport, private periods)
+                else if (classtime.style.backgroundColor && classtime.childNodes[1].nodeName == "DIV") {
+                    const classname = regExp.exec(classtime.childNodes[1].textContent.split("\n")[0])[1]
+                    localStorage.setItem(classname, classtime.style.backgroundColor)
+                }
+            }
+            //Cache is in unix for recaching
+            localStorage.setItem("cache-Colour", Date.now())
+        })
+        
+    }
+    //Async so other things do not try to access colours before this is done
+    return "done"
 }
--- compiled.js
+++ compiled.js
@@ -11,7 +11,7 @@ var regExp = /\(([^)]+)\)/;
 const RemoveTimetable = ["Before School", "Before School Sport", "Before School Programs", "Lunch Time Clubs", "Lunch Time Sport", "Period 5 Sport", "After School Clubs", "After School Sport", "After School"]
 // Conditions where "Click to view marks" will appear on feedback (uses str.includes())
 const ShowFeedbacks = ["(00", "[00", "(01", "[01", "(02", "[02", "(03", "[03", "(04", "[04", "(05", "[05", "(06", "[06", "(12", "[12"];
-var ColourEnabled = false
+var ColourEnabled = true
 window.addEventListener('load', (event) => {
     //Check for when the searchbar is there
     if (document.getElementById("message-list").children[1]) {
