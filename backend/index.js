5acbd1b40495e4e671a208ec6beb4b986226fce6
Zac McWilliam <zac@mcwilliam.net.au>
2022-03-10 22:42:09 +1100

    Backend clean up and improvements

--- backend/index.js
+++ backend/index.js
@@ -1,70 +1,58 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express"
 import connection from "express-myconnection"
+import cors from "cors"
+import mysql from "mysql2"
+import { config as envConfig } from "dotenv";
 
-import mysql from 'mysql2'
-
-const dbOptions = {
-    host: "192.168.64.2",
-    user: "silo",
-    password: "g/H_jm0y36m88y]5",
-    port: 3306,
-    database: 'themes'
-};
+envConfig();
 
 const app = express()
 
-app.use(connection(mysql, dbOptions, 'pool'));
+app.use(connection(mysql, {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
+ }, "pool"));
+
 app.use(cors());
 app.use(express.json())
-const SQLConnection = (mysql.createPool(dbOptions)).promise()
 
-const port = 3000
-app.get("/getusertheme/:userid", async function(req, res, next) {
+app.get("/theme/:userid", async function(req, res, next) {
     req.getConnection(async function(err, connection) {
         if (err) return next(err);
         const promisePool = connection.promise();
-        try { 
-            let [user] = await promisePool.execute("SELECT * FROM userthemes WHERE user = ?", [req.params.userid]);
-            if (user[0]) {
-                res.status(200).send(user[0]["theme"]);
-            }
-            else {res.status(200).send(false)}
-        } 
-        catch(error) { return res.send(500).send("internal server error"); }
+        try {
+            let [user] = await promisePool.execute("SELECT theme FROM userthemes WHERE user = ?", [req.params.userid]);
+            if (!user || user.length < 1) return res.status(404).send("Unknown User")
+            res.json(user[0]["theme"]);
+        }
+        catch(error) { return res.send(500); }
     })
 })
-app.post("/setusertheme/", async function(req, res, next) {
+
+app.post("/theme/:userid", async function(req, res, next) {
     req.getConnection(async function(err, connection) {
         if (err) return next(err);
         const promisePool = connection.promise();
         try { 
-            const user = req.body["user"]
+            const user = req.params.userid
             const theme = req.body["theme"]
-            if (!theme) {res.status(403).send("Theme is not defined"); return}
-            if (user) {
-                const [userfound] = await promisePool.execute("SELECT * FROM userthemes WHERE user = ?", [user]);
-                if (userfound[0]) {
-                    await promisePool.execute("UPDATE userthemes SET theme = ? WHERE userid = ${user};");
-                }
-                else {
-                    await promisePool.execute(`INSERT INTO userthemes (userid, themes) VALUES (${user}, ${theme});`);
-                }
+            if (!(/^-?\d+$/.test(user))) return res.status(400).send("Invalid User") // Ensures user ID is an integer
+            if (parseInt(user) < 0) return res.status(400).send("Invalid User")
+            if (!theme) return res.status(400).send("Invalid Theme")
+
+            const [existinguser] = await promisePool.execute("SELECT user FROM userthemes WHERE user = ?", [user]);
+            if (existinguser.length < 1) { 
+                await promisePool.execute("INSERT INTO userthemes (user, theme) VALUES (?, ?)", [user, JSON.stringify(theme)]);
+            } else {
+                await promisePool.execute("UPDATE userthemes SET theme = ? WHERE user = ?", [JSON.stringify(theme), user])
             }
-            else {
-                res.status(403).send("User is not defined")
-            }
+            res.sendStatus(200)
         } 
-        catch(error) {res.send(500).send("internal server error");return; }
-        // eturn res.send(500).send("internal server error")
+        catch (error) { return next(error) }
     })
 })
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-function guidGenerator() {
-    var S4 = function() {
-       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-    };
-    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
-}
+
+app.listen(process.env.PORT, () => { console.log(`App listening on port ${process.env.PORT}`) })
