350bb0236a6dbb984d274f074d5ec7347f6995be
Cowbiler <sebgtaylor@gmail.com>
2023-03-23 09:39:55 +1100

    intiial commit

--- chrome_ext/extension.js
+++ chrome_ext/extension.js
@@ -1,16 +1,9 @@
-chrome.storage.local.get(['onOrOff'], result => {
-  if (result.onOrOff) {
-    fetch(chrome.runtime.getURL("compiled.js")).then((r) => { return r.text() }).then((r) => {
-      var script = document.createElement('script');
-      var code = document.createTextNode('(function() { const forceEnableQOL = true; ' + r + '})();');
-      script.appendChild(code);
-      (document.head || document.body).appendChild(script);
-    })
-    fetch(chrome.runtime.getURL("darkmode.css")).then((r) => { return r.text() }).then((r) => {
-      var script = document.createElement('style');
-      var code = document.createTextNode(r);
-      script.appendChild(code);
-      (document.head || document.body).appendChild(script);
-    })
-  }
+fetch(chrome.runtime.getURL("darkmode.css")).then((r) => { return r.text() }).then((r) => {
+  var script = document.createElement('style');
+  var code = document.createTextNode(r);
+  document.styleSheets[1].disabled = true
+  document.getElementsByClassName("logo")[0].querySelector("img").src = 
+  script.appendChild(code);
+  (document.head || document.body).appendChild(script);
+
 })
