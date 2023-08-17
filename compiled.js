bca1a06fc59b524cebf41e7ff3b641e525c29ad9
Anyone Can Code <Yammmma@users.noreply.github.com>
2023-08-17 20:30:22 +1000

    Properly implementing Seb's fix

--- compiled.js
+++ compiled.js
@@ -51,6 +51,8 @@ function applyDark() {
     let script = document.createElement('link');
     script.rel = "stylesheet";
     script.href = DARKMODE_CSS_URL;
+
+    document.styleSheets[1].disabled = false;
     script.id = "darkmode-core";
     (document.head || document.body).appendChild(script);
 }
